c8bf45487c309e6576a0b326bbbe123a3f4c678c
kerenashih <44820372+kerenashih@users.noreply.github.com>
2018-11-06 22:53:38 +0100

    Add files via upload

---
+++ Sweetsourchicken.php
@@ -0,0 +1,89 @@
+<?php
+?>
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="reset.css">
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+    
+	<title> Sweet and Sour Chicken </title>
+ </head>
+  <body>
+	  <h1><ins> Sweet and sour chicken</ins></h1>
+    <?php require_once ("menu.php"); ?>
+	 <img src="http://natashaskitchen.com/wp-content/uploads/2014/12/Sweet-and-Sour-Chicken-13.jpg" alt="bild"> <br>
+	 
+	 <p class="Tot"> Cooking time 30-40 min </p>
+	 
+	 <h4>Ingredients:</h4>
+	 <ul>
+	      <li>3-4 Chicken file </li>
+	      <li>1/2 Onien </li>
+		  <li>2 Garlic cloves (pressed)</li> 
+		  <li>1 Fresh tomato</li> 
+		  <li>1 Veggie stock dice </li>
+		  <li>1/2 piece of cinnamon stick </li>
+		  <li>3-4 Cloves </li>
+		  <li>1 teaspoon Paprica powder </li>
+		  <li>1 1/2 teaspoon Curry </li>
+		  <li>1 pinch Salt </li>
+		  <li>1 pinch black pepper </li>
+		  <li>1/2 tablespoon Lemon juice </li>
+		  <li>4 tablespoons Sweet chilli sause </li>
+		  <li>2 tablespoons Chilli sause </li>
+		  <li>3 tablespoons Oil</li>
+		  <li>1 tablespoon Honey </li>
+	 </ul>
+	<br>
+	<br>
+	<h4> Serve this with rice... </h4>
+	<br>
+	 <h4>Ingredients:</h4>
+	 <ul>
+	     <li>4 dl Rice </li>
+	     <li>Oil </li>
+		 <li>1 tablespoon Curry </li>
+		 <li>8 dl water </li>
+		 <li>1 Garlic clove (pressed) </li>
+		 <li> Zest from 1/2 lemon </li>
+		 <li> Small piece Cinnamon stick </li>
+	</ul>
+	<h4>Directions:</h4>
+	<h5> Sweet sour Chicken: </h5> 
+	<ol> 
+	     <li> Fry the oniens in a few tablespoons of oil. </li>
+		<li> After the oniens are soft put in the garlic, cinnamon stick 
+		 and cloves. </li>
+		<li> Put in the chicken to fry. </li>
+		 <li>When the chicken is starting to cook, put in the 
+		 veggie stock dice and a pinch of salt and pepper.</li> 
+		<li> Let that cook a little bit </li>
+		<li> Put in the diced tomatoes. Let that cook for a few minutes. </li>
+		<li> Then add in the chilli sause, sweet chilli sause,<br> 
+		 paprica powder, curry, honey, lemon juice.</li>
+
+	</ol>
+	
+	<h5> Curry rice: </h5> 
+	   <ol> 
+		 <li> Fry the garlic and cinnamon stick in some oil. </li>
+		 <li>Put in the rice, lemon zest and curry then add water. </li>
+		 <li>Let it cook on low heat with a lid on. </li>
+	   </ol> 
+	   
+	<h4> ENJOY!</h4>
+	<div> 
+	    <div> 
+	     <?php require_once ("commentsectionC.php"); ?>
+
+	</div>
+
+	</div>
+      <br>
+      <br>
+		
+	<h5> Source: My own recipe </h5>
+	<h5> Picture: Natashaskitchen.com </h5>
+   </body>  
+ </html>  
