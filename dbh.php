45a5a439913b93674fb48bcb456d9bc9f7de7882
kerenashih <44820372+kerenashih@users.noreply.github.com>
2018-11-27 22:41:37 +0100

    Add files via upload

---
+++ dbh.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = 'localhost';
+$dbusername = 'root';
+$dbpassword = '';
+$dbname = 'commentsection';
+
+$conn = mysqli_connect($dbServername, $dbusername, $dbpassword, $dbname);
+
+?>
---
+++ menu.php
@@ -0,0 +1,17 @@
+
+	<div class="menu">
+		<?php
+		if (isset($_SESSION['id'])){
+
+		}
+		?>
+
+	<ul>
+		<li class="menu"><a href="index.php">Home</a></li>
+		<li class="menu"><a href="Calendar.php">Calendar</a></li>
+		<li class="menu"><a href="brownies.php">Brownies</a></li>
+		<li class="menu"><a href="Sweetsourchicken.php">Sweet & Sour Chicken</a></li>
+		<li class="menu"><a href="signup.php">Sign Up</a></li>
+
+	</ul>
+	</div>
